c5abab34ff87c5787b9010a54ac9f31b06b472cb
Hingue-Mouko Yanick-Arnaud <yanick-arnaud.hingue-mouko@inetum.com>
2024-07-14 01:06:48 +0200

    Add Swagger

--- src/produit/produit.controller.ts
+++ src/produit/produit.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ProduitsService } from './produit.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('produits')
 @Controller()
 export class ProduitsController {
   constructor(private readonly produitsService: ProduitsService) {}
   @Get('produits')
+  @ApiOperation({ summary: 'Get all produits' })
+  @ApiResponse({ status: 200, description: 'Return all produits.' })
   getAllProducts() {
     return this.produitsService.getAllProducts();
   }
